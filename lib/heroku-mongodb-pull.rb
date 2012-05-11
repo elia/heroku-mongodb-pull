fd9ad89163d1dd544870caaeab2e1273596c1014
Elia Schito <elia@schito.me>
2012-05-11 23:34:28 +0200

    Vanilla bundle gem

---
+++ lib/heroku-mongodb-pull.rb
@@ -0,0 +1,9 @@
+require "heroku-mongodb-pull/version"
+
+module Heroku
+  module Mongodb
+    module Pull
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/heroku-mongodb-pull/version.rb
@@ -0,0 +1,7 @@
+module Heroku
+  module Mongodb
+    module Pull
+      VERSION = "0.0.1"
+    end
+  end
+end
